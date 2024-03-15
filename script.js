6598307c6faa12ad4ea2728021c7451b981232b8
Gui-gt <30755guilherme@gmail.com>
2024-03-15 20:20:41 -0300

    att

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ function carregar(){
 var data = new Date()
 var hora = data.getHours()
 var res = document.getElementById('res')
+if(hora >= 0 && hora < 12){
+    res.innerHTML = `Bom dia`
+}else if(hora >= 12 && hora < 18){
+    res.innerHTML = `boa tarde`
+}else{
+    res.innerHTML = `Boa noite`
+}
 }
