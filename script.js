c68e4b0b5044f296fa0aa6cb0bc7559a01466d89
Gui-gt <30755guilherme@gmail.com>
2024-03-15 20:24:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var hora = data.getHours()
 var res = document.getElementById('res')
 if(hora >= 0 && hora < 12){
     res.innerHTML = `Bom dia`
-    document.body.style.
+    document.body.style.background = `pink`
 }else if(hora >= 12 && hora < 18){
     res.innerHTML = `boa tarde`
     document.body.style.background = `orange`
