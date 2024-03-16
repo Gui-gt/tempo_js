88498c6dcd8e03d2f00d6a43fb971cd453785246
Gui-gt <30755guilherme@gmail.com>
2024-03-16 10:03:25 -0300

    att

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var hora = data.getHours()
 var res = document.getElementById('res')
 if(hora >= 0 && hora < 12){
     res.innerHTML = `Bom dia`
+    Image.src = `https://tudosobreespiritualidade.com.br/wp-content/uploads/2020/11/b3-5.jpg`
     document.body.style.background = `pink`
 }else if(hora >= 12 && hora < 18){
     res.innerHTML = `boa tarde`
