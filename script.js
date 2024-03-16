223910e7a4d0f7419e05d2567a9a362847771322
Gui-gt <30755guilherme@gmail.com>
2024-03-16 10:05:15 -0300

    att

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function carregar(){
 var data = new Date()
 var hora = data.getHours()
 var res = document.getElementById('res')
+var img = document.getElementById('img')
 if(hora >= 0 && hora < 12){
     res.innerHTML = `Bom dia`
-    Image.src = `https://tudosobreespiritualidade.com.br/wp-content/uploads/2020/11/b3-5.jpg`
+    img.src = `https://tudosobreespiritualidade.com.br/wp-content/uploads/2020/11/b3-5.jpg`
     document.body.style.background = `pink`
 }else if(hora >= 12 && hora < 18){
     res.innerHTML = `boa tarde`
