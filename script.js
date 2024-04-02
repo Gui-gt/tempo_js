c9ff5f88386c64d04010f7042feafef2ee54bc0b
Gui-gt <30755guilherme@gmail.com>
2024-04-02 09:50:36 -0300

    atualizações

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function carregar(){
-var data = new Date()
-var hora = data.getHours()
-var res = document.getElementById('res')
-var img = document.getElementById('img')
-if(hora >= 0 && hora < 12){
-    res.innerHTML = `Bom dia`
-    img.src = `https://tudosobreespiritualidade.com.br/wp-content/uploads/2020/11/b3-5.jpg`
-    document.body.style.background = `pink`
-}else if(hora >= 12 && hora < 18){
-    res.innerHTML = `boa tarde`
-    document.body.style.background = `orange`
-}else{
-    res.innerHTML = `Boa noite`
-    document.body.style.background = `blue`
-}
+    let data = new Date()
+    let hora = data.getHours()
+    let res = document.getElementById('res')
+    if(hora >= 0 && hora < 12){
+        res.innerHTML = `Bom dia`
+        document.body.style.background = `pink`
+    }else if(hora >= 12 &&  hora < 18){
+        res.innerHTML = `boa tarde`
+        document.body.style.background = `orange`
+    }else{
+        res.innerHTML = `Boa noite`
+        document.body.style.background = `blue`
+    }
 }
